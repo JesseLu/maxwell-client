cb237dde263ced9b5db96809fbdf7a03c8cd868a
Jesse <mr.jesselu@gmail.com>
2013-07-16 16:10:24 -0700

    Donut mode works too!!!

--- example4_focusedbeam.m
+++ example4_focusedbeam.m
@@ -4,7 +4,8 @@ function [E, H, grid, eps] = example4_focusedbeam(varargin)
         % Parse inputs.
         %
 
-    options = my_parse_options(struct(  'flatten', false, ...
+    options = my_parse_options(struct(  'type', 'gaussian', ...
+                                        'flatten', false, ...
                                         'flen', 0), ...
                                 varargin, mfilename);
 
@@ -28,10 +29,34 @@ function [E, H, grid, eps] = example4_focusedbeam(varargin)
     [grid, eps] = maxwell_grid(omega, x, y, z);
                             % 'hires_box', {[0 -.4 0], [1 .4 1], [.05 .05 .05]});
 
-    J = maxwell_gaussian(grid, eps, [0 0 2], [8 8 -inf], 'x', options.flen, 0.9);
+    switch options.type
+        case 'gaussian'
+            J = maxwell_gaussian(grid, eps, [0 0 2], [8 8 -inf], 'y', options.flen, 0.9);
+        case 'donut'
+            mode_fun = zdonut([0 0 0], 0.8);
+            J = maxwell_fsmode(grid, eps, [0 0 2], [8 8 -inf], mode_fun, 'focal_length', options.flen);
+        otherwise
+            error('Type must either ''gaussian'' or ''donut''.');
+    end
    
 %     mode_fun = gaussian([0 0 0], 0.2, 1);
 %     J = maxwell_fsmode(grid, eps, [0 0 2], [8 8 -inf], mode_fun, 'focal_length', options.flen);
 
     [E, H] =  maxwell_solve(grid, eps, J);
-    maxwell_view(grid, eps, E, 'x', [nan 0 nan], 'field_phase', nan); % Visualize the excited waveguide.
+    maxwell_view(grid, eps, E, 'y', [nan 0 nan], 'field_phase', nan); % Visualize the excited waveguide.
+end
+
+
+function [fun] = zdonut(center, width)
+
+    function [E] = mode_fun(w, x, y, z)
+        r = sqrt(   (x - center(1)).^2 + ...
+                    (y - center(2)).^2) + 1e-10;
+        E = (w == 1) .* (y./r) .* sin(pi*r/width/2) .* (r < 2*width) + ...
+            (w == 2) .* (x./r) .* sin(pi*r/width/2) .* (r < 2*width);
+    end
+
+    fun = @mode_fun;
+    % fun = @(w, x, y, z) (w == pol) * ones(size(x));
+end
+        
