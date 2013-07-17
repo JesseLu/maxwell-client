19ee5c6a1e877d85244b171c387fe3735ec065f4
Jesse <mr.jesselu@gmail.com>
2013-07-17 12:03:01 -0700

    Got maxwell documentation generation to work.

---
+++ maxwell_help.m
@@ -0,0 +1,50 @@
+%% maxwell_help
+% Help for using Maxwell.
+
+%%% Functions
+%
+% * <maxwell_axb.html maxwell_axb> - Get the matrices and vectors associated with the electromagnetic wave equation.
+%
+% * <maxwell_box.html maxwell_box> - Used to insert a box of constant epsilon/mu into the simulation grid.
+%
+% * <maxwell_cyl.html maxwell_cyl> - Used to define a cylinder of a constant epsilon/mu in the simulation domain.
+%
+% * <maxwell_flux.html maxwell_flux> - Computes the Poynting vector through a finite plane of the simulation.
+%
+% * <maxwell_fsmode.html maxwell_fsmode> - Excite general free-space modes.
+%
+% * <maxwell_gaussian.html maxwell_gaussian> - Free-space Gaussian mode (0th-order).
+%
+% * <maxwell_grid.html maxwell_grid> - Create a simulation domain.
+%
+% * <maxwell_help.html maxwell_help> - Help for using Maxwell.
+%
+% * <maxwell_shape.html maxwell_shape> - Generic function to add constant material shapes to the simulation domain.
+%
+% * <maxwell_smooth_box.html maxwell_smooth_box> - Used to insert a "smoothed" box of constant epsilon/mu into the simulation grid.
+%
+% * <maxwell_smooth_cyl.html maxwell_smooth_cyl> - nction [cyl_fun] = maxwell_smooth_cyl(center, radius, height, varargin)
+%
+% * <maxwell_solve.html maxwell_solve> - Solves the electromagnetic wave equation.
+%
+% * <maxwell_solve_async.html maxwell_solve_async> - Initiate an electromagnetic solve and immediately return a callback function.
+%
+% * <maxwell_solve_eigenmode.html maxwell_solve_eigenmode> - Find the eigenmode based on an initial guess for E.
+%
+% * <maxwell_view.html maxwell_view> - Slice view of a field and/or structure.
+%
+% * <maxwell_wgmode.html maxwell_wgmode> - Solve for a waveguide mode.
+
+
+%%% Examples
+%
+% * <example0_waveguide.html example0_waveguide> - Waveguide example.
+%
+% * <example1_ring.html example1_ring> - Waveguide-coupled ring resonator.
+%
+% * <example2_metalmode.html example2_metalmode> - Mode of a metallic resonator
+%
+% * <example3_cavitymode.html example3_cavitymode> - 
+%
+% * <example4_focusedbeam.html example4_focusedbeam> - 
+
